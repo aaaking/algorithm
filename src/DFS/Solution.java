7253193e6a6d1f367c72e7442267eaea61c7d2b7
aaaking <zhouzhihui@bipa.io>
2022-02-18 00:54:17 +0800

    fs

--- src/DFS/Solution.java
+++ src/DFS/Solution.java
@@ -8,7 +8,8 @@ import java.util.Deque;
 import java.util.List;
 
 public class Solution {
-    int sum = 0;
+    int mVal = 0;
+    int mSum = 0;
 
     public static void main(String[] args) {
         Solution solution = new Solution();
@@ -40,32 +41,107 @@ public class Solution {
         }
         Deque<Integer> track = new ArrayDeque<Integer>();
         track.add(root.val);
-        sum += root.val;
+        mVal += root.val;
         backTraverse(root, target, track, res);
         return res;
     }
 
     private void backTraverse(TreeNode node, int target, Deque<Integer> track, List<List<Integer>> res) {
         if (node.left == null && node.right == null) {
-            if (sum == target) {
+            if (mVal == target) {
                 res.add(new ArrayList<>(track));
             }
             return;
         }
         if (node.left != null) {
             track.add(node.left.val);
-            sum += node.left.val;
+            mVal += node.left.val;
             backTraverse(node.left, target, track, res);
             track.pollLast();
-            sum -= node.left.val;
+            mVal -= node.left.val;
         }
         if (node.right != null) {
             track.add(node.right.val);
-            sum += node.right.val;
+            mVal += node.right.val;
             backTraverse(node.right, target, track, res);
             track.pollLast();
-            sum -= node.right.val;
+            mVal -= node.right.val;
         }
     }
 
+    /**
+     *         3
+     *     2       3
+     *        3        1
+     *
+     *            3
+     *        4       5
+     *     1   3        1
+     *
+     *            4
+     *         1
+     *           2
+     *             3
+     * @param root
+     * @return
+     */
+    public int rob(TreeNode root) {
+        if (root == null) return 0;
+        Deque<Integer> track = new ArrayDeque<Integer>();
+        track.add(root.val);
+        mSum += root.val;
+        backTraverse(root, track);
+        mVal = Math.max(mVal, mSum);
+        mSum = 0;
+        if (root.left != null) {
+            track.clear();
+            track.add(root.left.val);
+            mSum += root.left.val;
+            backTraverse(root.left, track);
+            mVal = Math.max(mVal, mSum);
+        }
+        if (root.right != null) {
+            track.clear();
+            track.add(root.right.val);
+            mSum += root.right.val;
+            backTraverse(root.right, track);
+            mVal = Math.max(mVal, mSum);
+        }
+        return mVal;
+    }
+
+    private void backTraverse(TreeNode node, Deque<Integer> track) {
+        if (node.left == null && node.right == null) {
+//            mVal = Math.max(mVal, mSum);
+            return;
+        }
+        if (node.left != null && node.left.left != null) {
+            track.add(node.left.left.val);
+            mSum += node.left.left.val;
+            backTraverse(node.left.left, track);
+            track.pollLast();
+//            mSum -= node.left.left.val;
+        }
+        if (node.left != null && node.left.right != null) {
+            track.add(node.left.right.val);
+            mSum += node.left.right.val;
+            backTraverse(node.left.right, track);
+            track.pollLast();
+//            mSum -= node.left.right.val;
+        }
+        if (node.right != null && node.right.right != null) {
+            track.add(node.right.right.val);
+            mSum += node.right.right.val;
+            backTraverse(node.right.right, track);
+            track.pollLast();
+//            mSum -= node.right.right.val;
+        }
+        if (node.right != null && node.right.left!= null) {
+            track.add(node.right.left.val);
+            mSum += node.right.left.val;
+            backTraverse(node.right.left, track);
+            track.pollLast();
+//            mSum -= node.right.left.val;
+        }
+    }
 }
