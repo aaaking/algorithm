3e2f2c7ee10ced3aea151dd50e7656f313a6ddf8
aaaking <zhouzhihui@bipa.io>
2022-02-20 13:21:58 +0800

    fs

---
+++ src/datastructor/Trie.java
@@ -0,0 +1,45 @@
+package datastructor;
+
+public class Trie {
+    public Trie[] children = new Trie[26];
+    public boolean isEnd;
+
+    public Trie() {
+    }
+
+    public void insert(String word) {
+        Trie node = this;
+        for (int i = 0; i < word.length(); i++) {
+            int c = word.charAt(i);
+            int index = c - 'a';
+            if (node.children[index] == null) {
+                node.children[index] = new Trie();
+            }
+            node = node.children[index];
+        }
+        node.isEnd = true;
+    }
+
+    public boolean search(String word) {
+        Trie node = searchPrefix(word);
+        return node != null && node.isEnd;
+    }
+
+    public boolean startsWith(String prefix) {
+        return searchPrefix(prefix) != null;
+    }
+
+    private Trie searchPrefix(String prefix) {
+        Trie node = this;
+        for (int i = 0; i < prefix.length(); i++) {
+            char ch = prefix.charAt(i);
+            int index = ch - 'a';
+            if (node.children[index] == null) {
+                return null;
+            }
+            node = node.children[index];
+        }
+        return node;
+    }
+
+}
