0c46f3cf9ca3957c956de10e9fb27c02ef6ea786
aaaking <zhouzhihui@bipa.io>
2022-03-04 23:37:55 +0800

    fs

---
+++ src/Main.java
@@ -0,0 +1,226 @@
+import datastructor.ListNode;
+
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        HJ54();
+    }
+
+    public static void HJ54() {
+        Scanner scanner = new Scanner(System.in);
+        String s = scanner.nextLine();
+        char[] chars = s.toCharArray();
+        Stack<String> stack = new Stack<>();
+        StringBuilder numBuilder = new StringBuilder();
+        // ((99+51)*14+26)*24      (7+5*4*3+6)    5-3+9*6*(6-10-2)
+        for (int i = 0; i < chars.length; i++) {
+            char c = chars[i];
+            if (Character.isDigit(c)) {
+                numBuilder.append(c);
+                if (i == chars.length - 1) {
+                    stack.push(numBuilder.toString());
+                }
+            } else {
+                if (numBuilder.length() > 0) {
+                    stack.push(numBuilder.toString());
+                }
+                stack.push(String.valueOf(c));
+                if (c == ')') {
+                    stack.pop(); // pop )
+                    String cal = calculate(stack);
+//                    stack.pop(); // pop (
+                    stack.push(cal);
+                }
+                numBuilder = new StringBuilder();
+            }
+        }
+        System.out.println("ori=" + stack);
+        if (stack.size() > 1) {
+            String cal = calculate(stack);
+            stack.push(cal);
+        }
+        System.out.println("after cal=" + stack);
+        String result = stack.pop();
+        System.out.println(result);
+    }
+
+    public static String calculate(Stack<String> stack) {
+        if (stack.size() < 3) {
+            return stack.pop();
+        }
+        Stack<String> temp = new Stack<>();
+        while (!stack.empty()) {
+            String s = stack.pop();
+            if (isPriority(s)) {
+                int a = Integer.parseInt(temp.pop());
+                int b = Integer.parseInt(stack.pop());
+                int value = calculate(a, b, s);
+                temp.push(String.valueOf(value));
+            } else if (!"(".equals(s)) {
+                temp.push(s);
+            }
+        }
+        System.out.println("temp=" + temp);
+        while (temp.size() >= 3) {
+            int a = Integer.parseInt(temp.pop());
+            String s = temp.pop();
+            int b = Integer.parseInt(temp.pop());
+            int value = calculate(a, b, s);
+            temp.push(String.valueOf(value));
+        }
+        return temp.pop();
+    }
+
+    public static int calculate(int a, int b, String operator) {
+        int ret = 0;
+        if ("+".equals(operator)) {
+            ret = a + b;
+        } else if ("-".equals(operator)) {
+            ret = a - b;
+        } else if ("*".equals(operator)) {
+            ret = a * b;
+        } else if ("/".equals(operator)) {
+            ret = a / b;
+        }
+        return ret;
+    }
+
+    public static boolean isPriority(String s) {
+        return "*".equals(s) || "/".equals(s);
+    }
+
+    public static boolean isNumeric(final String cs) {
+        if (cs == null || cs.length() <= 0) {
+            return false;
+        }
+        final int sz = cs.length();
+        for (int i = 0; i < sz; i++) {
+            if (!Character.isDigit(cs.charAt(i))) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static void HJ53() {
+        Scanner scanner = new Scanner(System.in);
+        while (scanner.hasNextInt()) {
+            int num = scanner.nextInt();
+            if (num == 1 || num == 2) {
+                System.out.println(-1);
+                continue;
+            } else if (num % 4 == 1 || num % 4 == 3) {
+                System.out.println(2);
+                continue;
+            } else if (num % 4 == 0) {
+                System.out.println(3);
+                continue;
+            } else if (num % 4 == 2) {
+                System.out.println(4);
+                continue;
+            }
+        }
+    }
+
+    public static void HJ51() {
+        Scanner scanner = new Scanner(System.in);
+        while (scanner.hasNext()) {
+            int n = scanner.nextInt();
+            ListNode[] array = new ListNode[n];
+            for (int i = 0; i < n; i++) {
+                ListNode node = new ListNode(scanner.nextInt());
+                array[i] = node;
+                if (i > 0) {
+                    array[i - 1].next = node;
+                }
+            }
+            int k = scanner.nextInt();
+            if (k <= 0) {
+                System.out.println(0);
+            } else {
+                ListNode node = array[n - k];
+                System.out.println(node.val);
+            }
+        }
+    }
+
+    public static void HJ46(String s, int k) {
+        System.out.println(s.substring(0, k));
+    }
+
+    public static void HJ40(String s) {
+        int alphaCnt = 0;
+        int spaceCnt = 0;
+        int digitCnt = 0;
+        int otherCnt = 0;
+        char[] chars = s.toCharArray();
+        for (int i = 0; i < chars.length; i++) {
+            char c = chars[i];
+            if (c == ' ') {
+                spaceCnt++;
+            } else if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z')) {
+                alphaCnt++;
+            } else if (Character.isDigit(c)) {
+                digitCnt++;
+            } else {
+                otherCnt++;
+            }
+        }
+        System.out.println(alphaCnt);
+        System.out.println(spaceCnt);
+        System.out.println(digitCnt);
+        System.out.println(otherCnt);
+    }
+
+    public static int HJ37(int n) {
+        if (n == 1) {
+            return 1;
+        }
+        if (n == 2) {
+            return 1;
+        }
+        if (n == 3) {
+            return 2;
+        }
+        int pre = HJ37(n - 1);
+        int pre2 = HJ37(n - 2);
+        return pre2 + pre;
+    }
+
+    public static void encrypt(String s) {
+        StringBuilder sb = new StringBuilder();
+        char[] chars = s.toCharArray();
+        for (int i = 0; i < chars.length; i++) {
+            sb.append(getOneChar(chars[i]));
+        }
+        System.out.println(sb);
+    }
+
+    public static String getOneChar(char c) {
+        // low case
+        if (c >= 'a' && c <= 'c') {
+            return "2";
+        } else if (c >= 'd' && c <= 'f') {
+            return "3";
+        } else if (c >= 'g' && c <= 'i') {
+            return "4";
+        } else if (c >= 'j' && c <= 'l') {
+            return "5";
+        } else if (c >= 'm' && c <= 'o') {
+            return "6";
+        } else if (c >= 'p' && c <= 's') {
+            return "7";
+        } else if (c >= 't' && c <= 'v') {
+            return "8";
+        } else if (c >= 'w' && c <= 'z') {
+            return "9";
+        } else if (c >= 'A' && c <= 'Z') {
+            c += 33;
+            if (c > 'z') {
+                c = 'a';
+            }
+        }
+        return Character.toString(c);
+    }
+}
