6bd9ce4ab2fb00faf925810e440de509609b4fd5
aaaking <zhouzhihui@bipa.io>
2022-04-22 23:20:51 +0800

    fs

---
+++ src/LeetCode.java
@@ -0,0 +1,17 @@
+public class LeetCode {
+    public static void main(String[] args) {
+        System.out.println("a".indexOf(""));
+        LeetCode leetCode = new LeetCode();
+    }
+
+    public int threeSumClosest(int[] nums, int target) {
+        int sum=nums[0]+nums[1]+nums[2];
+        int n=nums.length;
+        for (int i = 0; i < n-2; i++) {
+            int b=i+1;
+            int c=n-1;
+        }
+        return sum;
+    }
+
+}
