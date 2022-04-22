647a78332c404f82e06c5eaefc6945f8ff4642a9
aaaking <zhouzhihui@bipa.io>
2022-04-23 00:05:18 +0800

    fs

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,7 @@
 import datastructor.ListNode;
 
 import java.util.*;
-// HUAWEI机试题：
-// https://blog.csdn.net/Guesshat/article/details/122284160?utm_medium=distribute.pc_relevant.none-task-blog-2~default~baidujs_baidulandingword~default-0.queryctrv2&spm=1001.2101.3001.4242.1&utm_relevant_index=3
+
 public class Main {
     public static void main(String[] args) {
         HJ61();
