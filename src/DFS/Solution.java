a78309210f4f1bcce96186ad7595d48b7ed68dd4
aaaking <zhouzhihui@bipa.io>
2022-04-23 17:14:27 +0800

    fs

--- src/DFS/Solution.java
+++ src/DFS/Solution.java
@@ -16,25 +16,8 @@ public class Solution {
         Solution solution = new Solution();
     }
 
-    /**
-     * 剑指 Offer 34. 二叉树中和为某一值的路径
-     * 给你二叉树的根节点 root 和一个整数目标和 targetSum ，找出所有 从根节点到叶子节点 路径总和等于给定目标和的路径。
-     * <p>
-     * 叶子节点 是指没有子节点的节点。
-     * <p>
-     *  https://leetcode-cn.com/problems/er-cha-shu-zhong-he-wei-mou-yi-zhi-de-lu-jing-lcof/
-     * <p>
-     * 示例 1：
-     * <p>
-     * 树中节点总数在范围 [0, 5000] 内
-     * -1000 <= Node.val <= 1000
-     * -1000 <= targetSum <= 1000
-     * 注意：本题与主站 113 题相同：https://leetcode-cn.com/problems/path-sum-ii/
-     *
-     * @param root
-     * @param target
-     * @return res
-     */
+    // 剑指 Offer 34. 二叉树中和为某一值的路径 https://leetcode-cn.com/problems/er-cha-shu-zhong-he-wei-mou-yi-zhi-de-lu-jing-lcof/
+    // 给你二叉树的根节点 root 和一个整数目标和 targetSum ，找出所有 从根节点到叶子节点 路径总和等于给定目标和的路径。
     public List<List<Integer>> pathSum(TreeNode root, int target) {
         List<List<Integer>> res = new ArrayList<>();
         if (root == null) {
