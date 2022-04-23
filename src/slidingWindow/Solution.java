db6f641c20c4b4496b1019657e3c59ef336d8460
aaaking <zhouzhihui@bipa.io>
2022-04-23 15:33:35 +0800

    fs

--- src/slidingWindow/Solution.java
+++ src/slidingWindow/Solution.java
@@ -49,4 +49,24 @@ public class Solution {
         }
         return ret;
     }
+
+    public int maxVowels(String s, int k) {
+        int max = 0;
+        int sum = 0;
+        int left = 0, right = 0;
+        while (right < s.length()) {
+            sum += isVowel(s.charAt(right));
+            right++;
+            if (right >= k) {
+                max = Math.max(max, sum);
+                sum -= isVowel(s.charAt(right - k));
+            }
+        }
+        return max;
+    }
+
+    // 1456. 定长子串中元音的最大数目 https://leetcode-cn.com/problems/maximum-number-of-vowels-in-a-substring-of-given-length/
+    private int isVowel(char s) {
+        return s=='a' || s=='e' ||s=='i' ||s=='o' ||s=='u' ? 1:0;
+    }
 }
