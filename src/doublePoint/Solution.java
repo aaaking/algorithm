a81826c9e965482a22ef9cc6bf80f977e6a42481
aaaking <zhouzhihui@bipa.io>
2022-04-23 15:41:32 +0800

    fs

--- src/doublePoint/Solution.java
+++ src/doublePoint/Solution.java
@@ -1,14 +1,4 @@
 package doublePoint;
 
 public class Solution {
-    // 27. 移除元素 https://leetcode-cn.com/problems/remove-element/ 
-    public int removeElement(int[] nums, int val) {
-        int j = 0;
-        for (int i = 0; i < nums.length; i++) {
-            if (nums[i] != val) {
-                nums[j++] = nums[i];
-            }
-        }
-        return j;
-    }
 }
--- src/slidingWindow/Solution.java
+++ src/slidingWindow/Solution.java
@@ -5,6 +5,17 @@ import java.util.Deque;
 
 public class Solution {
 
+    // 27. 移除元素 https://leetcode-cn.com/problems/remove-element/
+    public int removeElement(int[] nums, int val) {
+        int j = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != val) {
+                nums[j++] = nums[i];
+            }
+        }
+        return j;
+    }
+
     // 1208. 尽可能使字符串相等 https://leetcode-cn.com/problems/get-equal-substrings-within-budget/
     public int equalSubstring(String s, String t, int maxCost) {
         int left = 0, right = 0;
