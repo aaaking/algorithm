e1efe83630d083a3879e43f9a1a7c8cbab43bb14
aaaking <zhouzhihui@bipa.io>
2022-04-23 18:43:36 +0800

    fs

--- src/datastructor/Tree.java
+++ src/datastructor/Tree.java
@@ -226,4 +226,20 @@ public class Tree {
         dfsTraverse(root.right);
     }
 
+    // 108. 将有序数组转换为二叉搜索树 https://leetcode-cn.com/problems/convert-sorted-array-to-binary-search-tree/
+    public TreeNode sortedArrayToBST(int[] nums, int start, int end) {
+        if (start > end) {
+            return null;
+        }
+        int mid = (end + start) / 2;
+        TreeNode head = new TreeNode(nums[mid]);
+        head.left = sortedArrayToBST(nums, start, mid - 1);
+        head.right = sortedArrayToBST(nums, mid + 1, end);
+        return head;
+    }
+
+    public TreeNode sortedArrayToBST(int[] nums) {
+        return sortedArrayToBST(nums, 0, nums.length-1);
+    }
+
 }
