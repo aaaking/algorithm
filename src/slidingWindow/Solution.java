cdcb0e99d9562ffffa75924deb6886ff749e4e73
aaaking <zhouzhihui@bipa.io>
2022-04-23 15:10:29 +0800

    fs

---
+++ src/slidingWindow/Solution.java
@@ -0,0 +1,9 @@
+package slidingWindow;
+
+public class Solution {
+
+    // 1208. 尽可能使字符串相等 https://leetcode-cn.com/problems/get-equal-substrings-within-budget/
+    public int equalSubstring(String s, String t, int maxCost) {
+
+    }
+}
