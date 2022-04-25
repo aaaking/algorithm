f3212982b6e7d44d21e9a977b0f1e3457cf6c854
aaaking <1059084407@qq.com>
2022-04-25 15:16:19 +0800

    Update Ufs.java

--- src/datastructor/Ufs.java
+++ src/datastructor/Ufs.java
@@ -8,59 +8,78 @@ import java.util.Map;
  * union find set, 并查集
  */
 public class Ufs {
+    public int[] parent;
+    public int[] rank;
+
     /**
-     * 连通分量的个数
+     * 指向的父结点的权值
      */
-    private int count;
-    private int[] parent;
-    private int[] rank;
+    private double[] weight;
+
+    public int cnt;
+
     public int maxRank = 1;
 
-    public int getCount() {
-        return count;
-    }
-
     public Ufs(int n) {
-        this.count = n;
+        cnt = n;
         parent = new int[n];
         rank = new int[n];
+        weight = new double[n];
         for (int i = 0; i < n; i++) {
             parent[i] = i;
             rank[i] = 1;
+            weight[i] = 1;
         }
     }
 
     private int find(int x) {
-        while (x != parent[x]) {
+        while (parent[x] != x) {
+            weight[x] *= weight[parent[x]];
             parent[x] = parent[parent[x]];
             x = parent[x];
         }
         return x;
+        // return x == parent[x] ? x : (parent[x] = find(parent[x]));
     }
 
-    public void union(int x, int y) {
+    public void merge(int x, int y) {
+        merge(x, y, 1);
+    }
+
+    public void merge(int x, int y, double timesP) {
         int xRoot = find(x);
         int yRoot = find(y);
         if (xRoot == yRoot) {
             return;
         }
-        
         int rankX = rank[xRoot];
         int rankY = rank[yRoot];
         if (rankX <= rankY) {
             parent[xRoot] = yRoot;
-            rank[yRoot]+=rankX;
+            rank[yRoot] += rankX;
             maxRank = Math.max(maxRank, rank[yRoot]);
+            weight[xRoot] = weight[y] * timesP / weight[x];
         } else {
             parent[yRoot] = xRoot;
-            rank[xRoot]+=rankY;
+            rank[xRoot] += rankY;
             maxRank = Math.max(maxRank, rank[xRoot]);
+            weight[yRoot] = weight[x] / timesP / weight[y];
         }
-        
-        count--;
+        --cnt;
     }
 
     public boolean isUnion(int x, int y) {
         return find(x) == find(y);
+    }
+
+    public Integer find(int x, Condition condition) {
+        while (parent[x] != x) {
+            x = parent[x];
+        }
+        return x;
+    }
+
+    interface Condition {
+        boolean condition(int x);
     }
 }
