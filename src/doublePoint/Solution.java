5fd0185d19c31a2919b0842d216324d0f49a8d4d
aaaking <1059084407@qq.com>
2022-10-20 15:32:52 +0800

    Update Solution.java

--- src/doublePoint/Solution.java
+++ src/doublePoint/Solution.java
@@ -298,4 +298,15 @@ public class Solution {
     private int isVowel(char s) {
         return s == 'a' || s == 'e' || s == 'i' || s == 'o' || s == 'u' ? 1 : 0;
     }
+    
+    // 11. 盛最多水的容器  https://leetcode.cn/problems/container-with-most-water/
+    public int maxArea(int[] height) {
+        int i = 0, j = height.length - 1, res = 0;
+        while (i < j) {
+            res = height[i] < height[j] ?
+                Math.max(res, (j - i) * height[i++]) :
+                Math.max(res, (j - i) * height[j--]);
+        }
+        return res;
+    }
 }
