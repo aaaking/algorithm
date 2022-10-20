ec84fa9fca52658ff7269128bfbe005fe4c09c88
aaaking <1059084407@qq.com>
2022-10-20 15:18:26 +0800

    Update ListNode.java

--- src/datastructor/ListNode.java
+++ src/datastructor/ListNode.java
@@ -142,4 +142,20 @@ public class ListNode {
         }
         return head;
     }
+    
+    public ListNode oddEvenList2(ListNode head) {
+        if (head == null) {
+            return head;
+        }
+        ListNode evenHead = head.next;
+        ListNode odd = head, even = evenHead;
+        while (even != null && even.next != null) {
+            odd.next = even.next;
+            odd = odd.next;
+            even.next = odd.next;
+            even = even.next;
+        }
+        odd.next = evenHead;
+        return head;
+    }
 }
