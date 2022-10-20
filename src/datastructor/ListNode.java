768b046870858183d6f03011df7552ba4af0a119
aaaking <1059084407@qq.com>
2022-10-20 16:12:44 +0800

    Update ListNode.java

--- src/datastructor/ListNode.java
+++ src/datastructor/ListNode.java
@@ -74,6 +74,31 @@ public class ListNode {
         }
         return ans;
     }
+    
+    public ListNode swapPairs(ListNode head) {
+        if(head == null || head.next == null){
+            return head;
+        }
+        ListNode next = head.next;
+        head.next = swapPairs(next.next);
+        next.next = head;
+        return next;
+    }
+    
+    public ListNode swapPairs(ListNode head) {
+        ListNode pre = new ListNode(0);
+        pre.next = head;
+        ListNode temp = pre;
+        while(temp.next != null && temp.next.next != null) {
+            ListNode start = temp.next;
+            ListNode end = temp.next.next;
+            temp.next = end;
+            start.next = end.next;
+            end.next = start;
+            temp = start;
+        }
+        return pre.next;
+    }
 
     public ListNode sortList(ListNode head) {
         return sortList(head, null);
