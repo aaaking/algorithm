c00ed86b1176c928fe38ce937f4bc36798482b43
aaaking <zhouzhihui@bipa.io>
2022-11-01 15:03:34 +0800

    fs

--- src/datastructor/ListNode.java
+++ src/datastructor/ListNode.java
@@ -83,7 +83,7 @@ public class ListNode {
             return head;
         }
         ListNode next = head.next;
-        head.next = swapPairs(next.next);
+        head.next = swapPairs2(next.next);
         next.next = head;
         return next;
     }
