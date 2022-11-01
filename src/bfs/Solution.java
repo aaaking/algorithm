edab90e6340579c6281678f22d5ca1d9043958a1
aaaking <zhouzhihui@bipa.io>
2022-11-01 15:57:59 +0800

    fs

--- src/bfs/Solution.java
+++ src/bfs/Solution.java
@@ -141,7 +141,7 @@ public class Solution {
         diameterOfBinaryTree(root.right);
         return diameter;
     }
-    // 二叉树的最大深度
+    // 104. 二叉树的最大深度 https://leetcode.cn/problems/maximum-depth-of-binary-tree/
     private int maxDepth(TreeNode root) {
         if (root == null) {
             return 0;
