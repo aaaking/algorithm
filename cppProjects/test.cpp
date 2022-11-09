c4c71701602e8a260c58efea92af5ba84fa0a3bb
aaaking <zhouzhihui@bipa.io>
2022-11-09 15:55:22 +0800

    fs

---
+++ cppProjects/test.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+class OrderOfFive {
+   public:
+      int operator=(const int n) {
+        int result = 1;
+        for (int i = 0; i < n; i++) {
+            result *= 5;
+        }
+        return result;
+      }
+};
+int main() {
+   OrderOfFive OrderOfFive;
+   cout << "1=" << (OrderOfFive=1) << endl;
+   cout << "2=" << (OrderOfFive=2) << endl;
+   cout << "3=" << (OrderOfFive=3) << endl;
+   cout << "4=" << (OrderOfFive=4) << endl;
+   cout << "5=" << (OrderOfFive=5) << endl;
+   cout << "5=" << (OrderOfFive=5) << endl;
+   cout << "5=" << (OrderOfFive=5) << endl;
+   cout << "5=" << (OrderOfFive=5) << endl;
+   return 0;
+}
---
+++ cppProjects/test.lua
@@ -0,0 +1,7 @@
+local info = { ["name"] = "zzh", ["age"] = 11 }
+
+if (info.play_mode) then
+    print("if" .. info.play_mode)
+else
+    print("else nil")
+end
