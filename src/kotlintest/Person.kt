174b749d54e11b9236efaa1a8790224d00d9ff93
zhouzhihui <wb_zhouzhihui@kuaishou.com>
2024-06-21 11:44:07 +0800

    xmlreader

--- src/kotlintest/Person.kt
+++ src/kotlintest/Person.kt
@@ -1,7 +1,12 @@
 package kotlintest
 
+import java.io.File
+
 class Person {
     fun test() {
         println("test kotlintest of person")
+        val projectPath = System.getProperty("user.dir")
+        val file = File("")
+        println("projectPath in kotlin=======" + projectPath + " file absolutepath=" + file.absolutePath)
     }
 }
