03e05954b27f91ab52f94d0ddddbf117c8ebd178
aaaking <zhouzhihui@kuaishou.com>
2025-03-10 05:46:17 +0800

    fs

--- python/draw_mah.py
+++ python/draw_mah.py
@@ -10,7 +10,8 @@
 #     Polygon(*dino_vectors)
 # )
 #
-
+import multiprocessing
+import sys
 import threading
 from math import sqrt, pi, ceil, floor
 import matplotlib
@@ -39,10 +40,28 @@ def draw_bar():
     for i in range(len(categories)):
         plt.text(i, values[i], str(values[i]), ha='center', va='bottom')
 
+    def show_plot_subprocess():
+        logg.log_red("subprocess show image thread=" + str(threading.currentThread()) + " is daemon=" + str(threading.currentThread().isDaemon()))
+        # 设置后端为 Agg
+        # matplotlib.use('Agg')
+        # 切换后端为 Agg
+        # plt.switch_backend('Agg')
+        # plt.bar(["RR", "TT"], [1000, -10]) # 这个柱状图会和住进程的追加
+        # plt.show()  # not work, show nothing, but savefig works
+        import time
+        time.sleep(2)
+        logg.log_red("subprocess exit")
+        sys.exit()
+    # 创建并启动进程
+    plot_process = multiprocessing.Process(target=show_plot_subprocess)
+    plot_process.start()
+
     # plt.show(block = False) # 这个方法，其他代码必须放在pause之前 才不会被阻塞，draw方法也有这个问题
     plt.draw()
+    logg.log_pink("save fig start")
     imgName = f"{DIR_MATH_PIC}/basic_bar_savefig_{timeformat()}.png"
-    plt.savefig(imgName, dpi=300, bbox_inches='tight')
+    # plt.savefig(imgName, dpi=300, bbox_inches='tight')
+    logg.log_pink("save fig end")
     plt.pause(-1)  # 保持图形窗口打开10秒, -1 infinite
 
     def show_plot_subthread():
@@ -51,6 +70,8 @@ def draw_bar():
     plot_thread = threading.Thread(target=show_plot_subthread)
     plot_thread.start()
     plot_thread.join()
+    plot_process.join()
+    plt.close()
 
 checkMathDir()
 if __name__ == '__main__':
