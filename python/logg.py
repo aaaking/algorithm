9a1465be2c8e8c8f6f5987d4a53cccf319813a0d
aaaking <zhouzhihui@kuaishou.com>
2025-03-10 01:29:41 +0800

    s

---
+++ python/logg.py
@@ -0,0 +1,14 @@
+def log_red(msg):
+    print('\033[1;31;40m%s\033[0m'  % msg)
+def log_green(msg):
+    print('\033[1;32;40m%s\033[0m'  % msg)
+def log_yellow(msg):
+    print('\033[1;33;40m%s\033[0m'  % msg)
+def log_blue(msg):
+    print('\033[1;34;40m%s\033[0m'  % msg)
+def log_pink(msg):
+    print('\033[1;35;40m%s\033[0m'  % msg)
+def log_sky(msg):
+    print('\033[1;36;40m%s\033[0m'  % msg)
+def log_grey(msg):
+    print('\033[1;37;40m%s\033[0m'  % msg)
