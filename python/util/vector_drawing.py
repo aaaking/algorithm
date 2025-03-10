2824f7a65f55da62aaeae355df1f7dc5d599f43f
aaaking <zhouzhihui@kuaishou.com>
2025-03-10 12:53:56 +0800

    ss

--- python/util/vector_drawing.py
+++ python/util/vector_drawing.py
@@ -14,6 +14,17 @@ purple = 'C4'
 orange = 'C2'
 gray = 'gray'
 
+"""
+yield 关键字在 Python 中用于定义生成器（generator）。生成器是一种特殊的迭代器，它允许你在函数内部逐步生成值，而不是一次性生成所有值并返回一个完整的列表。这使得生成器在处理大量数据时特别有用，因为它可以节省内存并提高性能。
+yield 的主要作用
+1. 延迟计算：
+生成器不会一次性计算所有的值，而是在每次调用 next() 方法时计算下一个值。这意味着你可以逐步处理数据，而不是一次性加载所有数据。
+2. 节省内存：
+由于生成器只在需要时生成值，因此它可以处理非常大的数据集，而不会因为一次性加载所有数据而导致内存不足。
+3. 简化代码：
+使用生成器可以使代码更加简洁和易读，因为你可以在一个函数中逐步生成值，而不是在一个函数中返回一个完整的列表。
+"""
+
 class Polygon():
     def __init__(self, *vertices, color=blue, fill=None, alpha=0.4):
         self.vertices = vertices
@@ -24,6 +35,47 @@ class Polygon():
         base_str = super().__str__()
         return base_str
 
+class Points():
+    def __init__(self, *vectors, color=black):
+        self.vectors = list(vectors)
+        self.color = color
+
+class Arrow:
+    def __init__(self, tip, tail = (0, 0), color = red):
+        self.tip = tip
+        self.tail = tail
+        self.color = color
+
+class Segment():
+    def __init__(self, start_point, end_point, color=blue):
+        self.start_point = start_point
+        self.end_point = end_point
+        self.color = color
+
+# helper function to extract all the vectors from a list of objects
+def extract_vectors(objects):
+    for object in objects:
+        typeObj = type(object)
+        if typeObj == Polygon:
+            for v in object.vertices:
+                yield v
+        elif typeObj == Points:
+            for v in object.vectors:
+                yield v
+        elif typeObj == Arrow:
+            yield object.tip
+            yield object.tail
+        elif typeObj == Segment:
+            yield object.start_point
+            yield object.end_point
+        else:
+            raise TypeError("Unrecognized object: {}".format(object))
+
 if __name__ == "__main__":
-    polygon = Polygon((1, 2), (3, 4))
-    print("polygon=" + str(polygon) + " id=" + hex(id(polygon)))
+    points = Points((1, 2), (3, 4))
+    print("points=" + str(points) + " id=" + hex(id(points)))
+    gen = extract_vectors([Points((1, 2), (3, 4)), Segment((1,2), (3,4))])
+    print(str(next(gen)))
+    print(str(next(gen)))
+    print(str(next(gen)))
+    print(str(next(gen)))
