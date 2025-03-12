9f8b45d93b39fa088e67ecf1f7489f206e3b9432
aaaking <zhouzhihui@kuaishou.com>
2025-03-12 21:14:26 +0800

    ss

--- python/util/vector_drawing.py
+++ python/util/vector_drawing.py
@@ -82,6 +82,95 @@ def draw2d(*objects, origin=True, axes=True, grid=(1, 1), nice_aspect_ratio=True
         print("save matplotlib end")
     plt.show()
 
+
+def draw3d(*objects, origin=True, axes=True, width=6, save_as=None, azim=None, elev=None, xlim=None, ylim=None, zlim=None, xticks=None, yticks=None, zticks=None, depthshade=False):
+    fig = plt.figure(4812094)
+    ax = fig.add_subplot(111, projection='3d')
+    ax.view_init(elev=elev, azim=azim)
+    all_vectors = list(extract_vectors_3D(objects))
+    if origin:
+        all_vectors.append((0, 0, 0))
+    xs, ys, zs = zip(*all_vectors)
+
+    max_x, min_x = max(0, *xs), min(0, *xs)
+    max_y, min_y = max(0, *ys), min(0, *ys)
+    max_z, min_z = max(0, *zs), min(0, *zs)
+
+    x_size = max_x - min_x
+    y_size = max_y - min_y
+    z_size = max_z - min_z
+
+    padding_x = 0.05 * x_size if x_size else 1
+    padding_y = 0.05 * y_size if y_size else 1
+    padding_z = 0.05 * z_size if z_size else 1
+
+    plot_x_range = (min(min_x - padding_x, -2), max(max_x + padding_x, 2))
+    plot_y_range = (min(min_y - padding_y, -2), max(max_y + padding_y, 2))
+    plot_z_range = (min(min_z - padding_z, -2), max(max_z + padding_z, 2))
+    ax.set_xlabel('x')
+    ax.set_ylabel('y')
+    ax.set_zlabel('z')
+
+    def draw_segment(start, end, color=black, linestyle='solid'):
+        xs, ys, zs = [[start[i], end[i]] for i in range(0, 3)]
+        ax.plot(xs, ys, zs, color=color, linestyle=linestyle)
+
+    if axes:
+        draw_segment((plot_x_range[0], 0, 0), (plot_x_range[1], 0, 0))
+        draw_segment((0, plot_y_range[0], 0), (0, plot_y_range[1], 0))
+        draw_segment((0, 0, plot_z_range[0]), (0, 0, plot_z_range[1]))
+
+    if origin:
+        ax.scatter([0], [0], [0], color='k', marker='x')
+
+    for object in objects:
+        if type(object) == Points3D:
+            xs, ys, zs = zip(*object.vectors)
+            ax.scatter(xs, ys, zs, color=object.color, depthshade=depthshade)
+
+        elif type(object) == Polygon3D:
+            for i in range(0, len(object.vertices)):
+                draw_segment(
+                    object.vertices[i],
+                    object.vertices[(i + 1) % len(object.vertices)],
+                    color=object.color)
+
+        elif type(object) == Arrow3D:
+            xs, ys, zs = zip(object.tail, object.tip)
+            a = FancyArrow3D(xs, ys, zs, mutation_scale=20, arrowstyle='-|>', color=object.color)
+            ax.add_artist(a)
+
+        elif type(object) == Segment3D:
+            draw_segment(object.start_point, object.end_point, color=object.color, linestyle=object.linestyle)
+
+        elif type(object) == Box3D:
+            x, y, z = object.vector
+            kwargs = {'linestyle': 'dashed', 'color': 'gray'}
+            draw_segment((0, y, 0), (x, y, 0), **kwargs)
+            draw_segment((0, 0, z), (0, y, z), **kwargs)
+            draw_segment((0, 0, z), (x, 0, z), **kwargs)
+            draw_segment((0, y, 0), (0, y, z), **kwargs)
+            draw_segment((x, 0, 0), (x, y, 0), **kwargs)
+            draw_segment((x, 0, 0), (x, 0, z), **kwargs)
+            draw_segment((0, y, z), (x, y, z), **kwargs)
+            draw_segment((x, 0, z), (x, y, z), **kwargs)
+            draw_segment((x, y, 0), (x, y, z), **kwargs)
+        else:
+            raise TypeError("Unrecognized object: {}".format(object))
+
+    if xlim and ylim and zlim:
+        plt.xlim(*xlim)
+        plt.ylim(*ylim)
+        ax.set_zlim(*zlim)
+    if xticks and yticks and zticks:
+        plt.xticks(xticks)
+        plt.yticks(yticks)
+        ax.set_zticks(zticks)
+
+    if save_as:
+        plt.savefig(save_as)
+    plt.show()
+
 def test_draw_easy_math(save_as=None):
     datax = np.linspace(0, 30, 200) # [0,`10]区间的线性增长的100个数字,  # xsin是数组[]不是元组()
     cos = Cos(datax)
@@ -196,6 +285,7 @@ if __name__ == "__main__":
     x = np.linspace(-10, 10, 100)
     # draw_math_func(x, (4 - x**3) ** (1/4))
     # draw_math_func(x, (1 - x ** (-1)) ** (-1))
+    draw3d(Points3D((2,2,2),(1,-2,-2)))
 
     # 关闭交互模式（可选）
     plt.ioff()
