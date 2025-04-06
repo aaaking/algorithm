1220f105617d35fc034f2fc7de3c890fd0b5141a
zhouzhihui <wb_zhouzhihui@kuaishou.com>
2025-04-06 21:40:26 +0800

    fs

--- src/math/MathDemo.java
+++ src/math/MathDemo.java
@@ -8,20 +8,29 @@ public class MathDemo {
         testEgyptFraction(2, 15);
         testEgyptFraction(5, 6);
         testEgyptFraction(8, 11);
+
+        testEgyptFraction(17, 43);
+        testEgyptFraction(4, 83);
     }
 
     public static List<Integer> testEgyptFraction(int a, int b) {
         List<Integer> ret = new ArrayList<>();
         while (a > 1) {
             int p = b / a;
-            ret.add(p + 1);
-            a = a + a * p - b;
-            b = b * (p + 1);
-            int gcd = gcd(a, b);
-            if (gcd > 1) {
-                a = a / gcd;
-                b = b / gcd;
+            if (b % a == 0) {
+                b = p;
+                a = 1;
+                break;
+            } else {
+                ret.add(p + 1);
+                a = a + a * p - b;
+                b = b * (p + 1);
             }
+//            int gcd = gcd(a, b);
+//            if (gcd > 1) {
+//                a = a / gcd;
+//                b = b / gcd;
+//            }
         }
         ret.add(b);
         System.out.println(ret);
